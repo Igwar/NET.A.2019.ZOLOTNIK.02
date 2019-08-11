0c35107899d4d04fe7e7225cd370a9006dfe1dd8
Igwar <43217217+Igwar@users.noreply.github.com>
2019-08-11 15:58:13 +0300

    Add files via upload

---
+++ FindNthRoot/FindNthRoot/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FindNthRoot
+{
+    public static class Find
+    {
+       public static double FindRoot(double a, double b, double eps)
+        {
+            var x0 = a / b;
+            var x1 = (1 / b) * ((b - 1) * x0 + a / Math.Pow(x0, (int)b - 1));
+
+            while (Math.Abs(x1 - x0) > eps)
+            {
+                x0 = x1;
+                x1 = (1 / b) * ((b - 1) * x0 + a / Math.Pow(x0, (int)b - 1));
+            }
+
+            return x1;
+        }
+
+    }
+}
