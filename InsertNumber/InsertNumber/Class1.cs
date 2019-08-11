13455bcda52c674cac5080e77708937fcbaba290
Igwar <43217217+Igwar@users.noreply.github.com>
2019-08-11 15:56:37 +0300

    Add files via upload

---
+++ InsertNumber/InsertNumber/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsertNumber
+{
+    public class InsertNum
+    {
+        public static int Insert(int num1, int num2, int i, int j)
+        {
+            if (i < 0 || j < 0 || i > 31 || j > 31)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            if (i > j)
+            {
+                throw new ArgumentException();
+            }
+
+            int mask = ((2 << (j - i)) - 1) << i;
+            return (num1 & ~mask) | ((num2 << i) & mask);
+        }
+    }
+}
