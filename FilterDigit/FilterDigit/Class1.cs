a7a7ff32abe6c1c68f4f82a1a0cf9a0936aebffe
Igwar <43217217+Igwar@users.noreply.github.com>
2019-08-11 15:57:40 +0300

    Add files via upload

---
+++ FilterDigit/FilterDigit/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilterDigit
+{
+    public class filtr
+    {
+        public static void findnimber(int number, ref List<int> numbers)
+        {
+            List<int> result = new List<int>();
+
+            foreach (var h in numbers)
+            {
+
+                string str = h.ToString();
+                foreach (char b in str)
+                {
+                    int c = (int)Char.GetNumericValue(b);
+                    if (c == number)
+                    {
+                        result.Add(h);
+                        break;
+                    }
+                }
+
+            }
+            numbers = result;
+
+
+        }
+    }
+}
